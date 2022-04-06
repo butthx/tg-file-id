7d9ff473b89c8f25c7cfecce26ba5d9ffb743d5b
butthx <68604099+butthx@users.noreply.github.com>
2022-04-06 10:00:17 +0700

    prebuild with tgsnake

--- test.js
+++
@@ -1,4 +0,0 @@
-let f = require('./dist/src/index');
-
-console.assert(f.decodeFileId("AwACAgQAAxkBAAEE3SZgO-PbHlWtxRt5cPWvXlGRWHXM3AACuwgAAj0d4FF_jv-i_-7iQR4E").fileType === 'voice','Check fileType');
-console.assert(f.decodeUniqFileId('AgADuwgAAj0d4FE').type==='document','check type')
